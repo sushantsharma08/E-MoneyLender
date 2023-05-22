0f71771b110669fec236f3d2402e73d250401ca3
Sushant Sharma <ssushantlb08@gmail.com>
2023-05-23 04:37:53 +0530

    form made

--- Client/E-moneylender/src/Form.jsx
+++ Client/E-moneylender/src/Form.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Form = () => {
+    const [name, setName] = useState();
+    const [fathername, setFathersname] = useState();
+    const [gender, setGender] = useState();
+    const [loanAmount, setLoanAmount] = useState(0);
+    const [totalAmount, setTotalAmount] = useState(0);
+    const [monthlyInstalment, setMonthlyInstalment] = useState(0);
+    
+    
+    const setValues= (amount)=>{
+        let P=amount.target.value;
+        let R=18/12/100;
+        let x=1+R;
+        let x1 = P*R*Math.pow(x,12);
+        let x2 = Math.pow(x,12)-1;
+        let emi = Math.ceil(x1/x2);
+            setLoanAmount(P);
+            setMonthlyInstalment(emi);
+            setTotalAmount(emi*12)
+    }
+    const showdata = ()=>{
+        console.log(name,fathername,gender);
+    }
+
   return (
     <div className='form'>
-        <label htmlFor="name">Name</label> <input id='name' type="text" />
+        <label htmlFor="name">Name</label> <input id='name' type="text" onChange={(e)=>{setName(e.target.value)}} />
         <br />
-        <label htmlFor="fathername">Father Name</label> <input id='fathername' type="text" />
-        <br />
-        <label htmlFor="mothername">Mother Name</label> <input id='mothername' type="text" />
+        <label htmlFor="fathername">Father Name</label> <input id='fathername' type="text" onChange={(e)=>{setFathersname(e.target.value)}} />
         <br />
 
         <label htmlFor="gender">Gender</label> 
-        <select name="gender" id="gender">
-        <option value="" default disabled>Select</option>
+        <select name="gender" id="gender" onClick={(e)=>{setGender(e.target.value)}}>
+        <option value="" selected disabled>Select</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
         </select>
@@ -24,20 +45,20 @@ const Form = () => {
         <br />
 
         <label htmlFor="Amount">Loan Amount</label> 
-        <input type="number" name="loanamount" id="Amount"/>
+        <input type="number" name="loanamount" id="Amount" onChange={(e)=>{setValues(e)}} />
         <br />
 
-        <label htmlFor="Amount">Monthly Installment</label> 
-        <input type="number" name="loanamount" id="Amount"/>
+        <label htmlFor="monthlyinstalment">Monthly Installment</label> 
+        <input type="number" disabled name="loanamount" id="Amountmonthlyinstalment" value={monthlyInstalment} />
         @18% per month
         <br />
 
         <label htmlFor="Amount">Total Amount</label> 
-        <input type="number" name="totalamount" id="totalAmount"/>
+        <input type="number" disabled name="totalamount" id="totalAmount" value={totalAmount} />
         <br />
 
         <label htmlFor="missedamount">If missed installment</label> 
-        <input type="number" name="missed" id="missedamount"/>
+        <input type="number" disabled name="missed" id="missedamount"/>
         <br />
 
         <label htmlFor="image">Upload Image</label> 
@@ -47,6 +68,8 @@ const Form = () => {
         <label htmlFor="signature">Upload Signature</label> 
         <input type="file" name="signature" id="signature"/>
         <br />
+
+        <button onClick={showdata} >save</button>
         
     </div>
   )
