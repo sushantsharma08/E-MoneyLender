62d2211ba947c522578f220965b8d17a036941fb
Sushant Sharma <ssushantlb08@gmail.com>
2023-07-19 00:28:23 +0530

    update ui

--- Client/E-moneylender/src/page/SearchClients.jsx
+++ Client/E-moneylender/src/page/SearchClients.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Popup from './Popup';
 import {
@@ -11,9 +11,10 @@ const queryClient = new QueryClient()
 
 const SearchClients = () => {
 
-  // const [Users, setUsers] = useState()
+  // Hooks
+
   const [SearchName, setSearchName] = useState()
-  const [showModal, setShowModal] = useState(false)
+  const [showClientDetailsModal, setShowClientDetailsModal] = useState(false)
   const [UserList, setUserList] = useState([])
 
   const { isLoading, error, data } = useQuery({
@@ -24,11 +25,62 @@ const SearchClients = () => {
       ),
   });
 
+  // Functions
+
+  const OpenUserDetails = (e) => {
+    setSearchName(e);
+    setShowClientDetailsModal(true)
+  }
+
+  const OpenClientEdit = (e) => {
+    setSearchName(e);
+    set
+  }
+
+  const CloseAccount = (e) => {
+    const id = e.target.id;
+    axios.delete(`https://e-money-lender-back.vercel.app/client/removeClient/${id}`);
+    setTimeout(() => {
+      window.location.reload();
+      clearTimeout()
+    }, 1000);
+  }
+
+  //dynamic UI from useQuery output
+
   if (isLoading) return <section id='client' style={{ position: "relative" }}>
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", paddingTop: "100px" }}>
       <div className="userdatadiv">
-        <h2>Loading...</h2>
+        <div class="boxes">
+          <div class="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div class="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div class="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div class="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+
+
       </div>
+      <h2>Loading...</h2>
     </div>
   </section>
 
@@ -39,18 +91,8 @@ const SearchClients = () => {
       </div>
     </div>
   </section>
-  console.log(data);
 
-  // const searchUserbyName = async (e)=>{setSearchName(e.target?.value);}
-
-  const searchUser = (e) => {
-    setSearchName(e);
-    setShowModal(true)
-  }
-  const checkName = (e) => {
-    const id = e.target.id;
-    axios.delete(`https://e-money-lender-back.vercel.app/client/removeClient/${id}`)
-  }
+  // Main 
 
   return (
     <section id='client' >
@@ -71,14 +113,14 @@ const SearchClients = () => {
             </thead>
             <tbody>
               {data?.map((user, index) =>
-                <tr key={index} className="userDetails" style={{ display: "flex", cursor: "pointer" }}>
+                <tr key={index} className="userDetails" style={{ display: "flex"}}>
                   <td className="tabledata index index_data">{index + 1}</td>
                   <td className="tabledata personal"><span className='name'>{user.name}</span><br /><span className='phone'><img src="/images/phone.png" alt="" width="12px" />{user?.phone}</span></td>
                   <td className="tabledata FatherName personal2"><span>{user.fathername}</span><br /></td>
                   <td className="tabledata adhaar">
                     <span className="id">
-                       <img src="/images/id.png" alt="" width="20px" />
-                       {user.adhaar}</span>
+                      <img src="/images/id.png" alt="" width="20px" />
+                      {user.adhaar}</span>
                   </td>
                   <td className="tabledata LoanDetails">
                     <span>
@@ -89,9 +131,12 @@ const SearchClients = () => {
                       <br />
                       <b>{user.remainingamount}</b></span></td>
                   <td className="tabledata status" style={{ color: user.remainingamount > 0 ? "green" : "red", textAlign: "center" }}>
-                    {user.remainingamount > 0 ? <span onClick={() => searchUser(user.name)} style={{ backgroundColor: "rgba(147, 209, 147, 0.359)" }}>Active</span> : <span style={{ backgroundColor: "rgba(241, 170, 170, 0.753)" }} id={user._id} onClick={(e) => checkName(e)}>Close Account</span>
+                    {user.remainingamount > 0 ?
+                      <span onClick={() => OpenUserDetails(user.name)} style={{ backgroundColor: "rgba(147, 209, 147, 0.359)" }}>Active</span>
+                      : <span style={{ backgroundColor: "rgba(241, 170, 170, 0.753)" }} id={user._id} onClick={(e) => CloseAccount(e)}>Close Account</span>
                     }
                   </td>
+                  <td className="tabledata Edit" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}><span className='EditBtnSpan'>Edit</span></td>
                 </tr>
               )}
             </tbody>
@@ -100,7 +145,7 @@ const SearchClients = () => {
 
       </div>
       <QueryClientProvider client={queryClient}>
-        <Popup show={showModal} clientName={SearchName}/>
+        <Popup show={showClientDetailsModal} clientName={SearchName} />
       </QueryClientProvider>
 
     </section>
