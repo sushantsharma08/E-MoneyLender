2e12437d91bcf7caf07cebea970880bcf8ebe9f5
Sushant Sharma <ssushantlb08@gmail.com>
2023-07-19 14:49:03 +0530

    server for auth

--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username:{type:String,required:true,unique:true},
-    password:{type:String,required:true}
+    username:{type:String,required:true},
+    password:{type:String,required:true,minLength:8},
+    name:{type:String,required:true,maxLength:225},
+    email:{type:String,required:true},
+    phone:{type:Number,required:true,length:10},
+    adhaar:{type:Number,required:true,length:12,unique:true},
+    panId:{type:String,required:true,unique:true}    
 });
 
 export const UserModel = mongoose.model("users",UserSchema)
--- Server/src/routes/user.js
+++ Server/src/routes/user.js
@@ -6,37 +6,37 @@ import { UserModel } from "../models/User.js";
 
 const router = express.Router();
 
-router.post("/register",async(req,res)=>{
-    const {username,password}=req.body;
+router.post("/register", async (req, res) => {
+    const { username, password, name, email, phone, adhaar, panId } = req.body;
 
-    const user = await UserModel.findOne({username});
-    if(user){
-        return res.json({"message":"user already exists"});
+    const user = await UserModel.findOne({ adhaar, panId });
+    if (user) {
+        return res.json({ "message": "user already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 8);
-    const newUser = new UserModel({username,password: hashedPassword});
+    const newUser = new UserModel({ username, password: hashedPassword, name, email, phone, adhaar, panId });
     await newUser.save();
-    res.json({message:"user registered successfully"});
+    res.json({ message: "user registered successfully" });
 });
 
-router.post("/login",async(req,res)=>{
-    const {username,password}=req.body;
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
 
-    const user = await UserModel.findOne({username});
+    const user = await UserModel.findOne({ username });
 
-    if(!user){
-        return res.json({message:"User Doesn't Exist!"})
+    if (!user) {
+        return res.json({ message: "User Doesn't Exist!" })
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password); ;
+    const isPasswordValid = await bcrypt.compare(password, user.password);;
 
-    if(!isPasswordValid){
-        return res.json({message:"User Password not valid!!!"})
+    if (!isPasswordValid) {
+        return res.json({ message: "User Password not valid!!!" })
     }
 
-    const token = jwt.sign({id:user._id},"secret");
-    res.json({token,userID: user._id })
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id })
 
 });
 
-export { router as UserRouter}
+export { router as UserRouter }
