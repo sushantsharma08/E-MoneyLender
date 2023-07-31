6f57f848f0228b974c97fc035eaf5159ac33b441
Sushant Sharma <ssushantlb08@gmail.com>
2023-08-01 00:09:56 +0530

    Update client.js

--- Server/src/routes/client.js
+++ Server/src/routes/client.js
@@ -3,7 +3,7 @@ import { ClientModel } from "../models/Client.js";
 // import client from ('twilio')("AC0eabea656803bc0bd73addb3b3c845ea", "64d0a8ab1e5bf1d4efd619c4ea5bd43a");
 // import * as Twilio from 'twilio';
 // import * as lib from "lib";
-import lib from 'twilio';
+// import lib from 'twilio';
 // import * as dotenv from "dotenv";
 // dotenv.config({path:"./config.env"});
 
